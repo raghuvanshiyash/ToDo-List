d36bdfdab86c89ea1eb6dd462b894935d169a617
raghuvanshiyash <raghuvanshiyash2110@gmail.com>
2022-06-14 22:09:16 +0530

    ToDo List, First project of React

---
+++ src/MyComponents/AddTodo.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+export const AddTodo = ({ addTodo }) => {
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+
+
+    const submit = (e) => {
+        e.preventDefault();
+        if (!title || !desc) {
+            alert("Title or Description cannot be blank");
+        }
+        else {
+            addTodo(title, desc);
+            setTitle("");
+            setDesc("");
+        }
+    }
+    return (
+        <div className="container my-3">
+            <h3>Add a ToDo</h3>
+            <form onSubmit={submit}>
+                <div className="mb-3">
+                    <label htmlFor="title" className="form-label"><b>ToDo Title</b></label>
+                    <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className="form-control" id="title" aria-describedby="emailHelp" />
+
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="desc" className="form-label"><b>ToDo Description</b></label>
+                    <input type="text" value={desc} onChange={(e) => setDesc(e.target.value)} className="form-control" id="desc" />
+                </div>
+                <button type="submit" className="btn btn-sm btn-success"><b>Add ToDo</b></button>
+            </form>
+        </div>
+    )
+}
